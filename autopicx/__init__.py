821dc28e6f9ef956e92def3f01dd394a471d12d3
AS TG[ONLINE] <126592884+TGxIRFAN@users.noreply.github.com>
2023-06-25 19:55:03 +0530

    Update __init__.py

--- autopicx/__init__.py
+++ autopicx/__init__.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 API_ID = int(environ.get("API_ID", "15762061"))
 API_HASH = environ.get("API_HASH", "364d3fa8f95aae815d62cf981ab1afe3")
-SESSION = environ.get("SESSION", "1BVtsOGYBu0-hA3inzrawesjRpPhCTUrwPIpPaDnp6TEk7mqdJbvlrWxJJyYJTCJmRdLJ4eVz4ZuIpoGxR5nAJVXZSkzDgrV3y71OFWdKc74KgSotkFbHdxgfgCJOibdrjCd9O4FzR08o1uX66vvJ3iYmvMdAOq_Wiz3rKCdunuirR02a9sekUFElLhb70H562p2ruw9CBbIOjBMM-yOTxRUtIF8qP4k9DGWhq4mDWZ2jUaHIqK3maEBfblZPDICv5gMP6LY2dRV4yqM0X1f70AqQUDrG_vDmxbFVhwb0EcVjbo4_hrm12qQyMRNy4c8hKCDh8QaD99woc8lmg59EZe9Q6TCUqPY")
+SESSION = environ.get("1BVtsOGYBu0-hA3inzrawesjRpPhCTUrwPIpPaDnp6TEk7mqdJbvlrWxJJyYJTCJmRdLJ4eVz4ZuIpoGxR5nAJVXZSkzDgrV3y71OFWdKc74KgSotkFbHdxgfgCJOibdrjCd9O4FzR08o1uX66vvJ3iYmvMdAOq_Wiz3rKCdunuirR02a9sekUFElLhb70H562p2ruw9CBbIOjBMM-yOTxRUtIF8qP4k9DGWhq4mDWZ2jUaHIqK3maEBfblZPDICv5gMP6LY2dRV4yqM0X1f70AqQUDrG_vDmxbFVhwb0EcVjbo4_hrm12qQyMRNy4c8hKCDh8QaD99woc8lmg59EZe9Q6TCUqPY")
 TIME = int(environ.get("TIME", "60"))
 CHANNEL_ID = int(environ.get("CHANNEL_ID", "-1001900598149"))
 
