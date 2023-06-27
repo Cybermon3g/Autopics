fb2d73688d0a07eca41746b7af3f8c887d6caff6
Cybermon3g <134679332+Cybermon3g@users.noreply.github.com>
2023-06-27 19:47:48 +0530

    Update __init__.py

--- autopicx/__init__.py
+++ autopicx/__init__.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 API_ID = int(environ.get("API_ID", "28915632"))
 API_HASH = environ.get("API_HASH", "19b37f365054be433a4d75e949ffde39")
-SESSION = environ.get("1BVtsOGwBu3oSY5v04T3O9s8jssGknT1dE2ditImHmpz9h18PNr6oY89vZWu-iDIU95PPzH4cK9VUjmCI5dVjDkDZIsN-hvaWfDAnvsyrsGOr7PsPYOc4HTIAxe4Kk_pXxrbPpCgJSxj8VjKDCFxnvT9_TL1mt-Ng5fTHYLHonn-RAdRF_l7JrsDH_ESQwsTHOgnGmkITkcOUyUHvMCO3yLEvRJpTgT1tmB8CAgS7Ryll9e0XAzE58hU7ZuARzJybSeEBFrEdkHqLuwJRveCK1DxRmBlOUKnkM6o4ZHohsZIEddEx3jPktO_A6g8rOhV5qUoh1oUB3Lc63wwxGIsXgZ-L50Bs9sI=")
+SESSION = environ.get("1BVtsOHkBu4T1MnWRhDI4hB9NDIEGyuzVhJAtAdXps_1Pb3I4RY7RVCMTupy8GyYrGBQ16k9Zf3-aQyudPNKViF-7F2fhuaKjtIiEGb9C4jg0Qqrm4XkqOD-UpJ7BvNvJk6a-cVSXlP8jYnL6NsMvlHY-80m7_qEUPw5IduWdAQrz66pKZ-19UkKwEzZR7U1GmgwY02V8o7V2hGSkmDMUO7RMbZN1eTXxsMSaL2BAF_tzSC9U0YSn6TJTg02DUlQa_zbp6oc1RsvankLhAuhZPH-Afy09k3TVs4_p3-ENpL6DGCepaokCC2KZCHz-YW2kt2TvCaVAYweNHyfO8e7Gm5gIq9osaW8=")
 TIME = int(environ.get("TIME", "60"))
 CHANNEL_ID = int(environ.get("CHANNEL_ID", "-1001227879771"))
 
